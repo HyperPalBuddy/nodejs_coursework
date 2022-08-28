d00fc3f64022b46bb397c58a850585581efb91cb
Saiteja Alapati <61974182+HyperPalBuddy@users.noreply.github.com>
2022-08-28 14:19:13 +0530

    added 4th class files

---
+++ 4-os-module.js
@@ -0,0 +1,27 @@
+const os = require('os')
+
+//Info on User
+const user = os.userInfo()
+console.log(user)
+//Prints System Uptime
+//console.log(os.uptime())
+
+const currentOS = {
+    name: os.hostname(),
+    type:os.type(),
+    arch:os.arch(),
+    release: os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem(),
+    uptime:os.uptime,
+
+}
+
+console.log(`The name of the system is: ${currentOS.name}`)
+console.log(`The type of the system OS is: ${currentOS.type}`)
+console.log(`The release of the system OS is: ${currentOS.name}`)
+console.log(`The name of the system architecture is: ${currentOS.arch}`)
+console.log(`The amount of total memory is: ${currentOS.totalMem} in bytes`)
+console.log(`The amount of free memory is: ${currentOS.freeMem} in bytes`)
+console.log(`The uptime of the system is: ${currentOS.uptime} in seconds`)
+
