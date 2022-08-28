9a5a99bbfd7f4004e7f8490c6ceec71e5440fd89
Saiteja Alapati <61974182+HyperPalBuddy@users.noreply.github.com>
2022-08-28 11:29:58 +0530

    added 2nd class file

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+// Globals
+
+// __dirname -- Gives current directory
+// __filename -- Gives the filename
+// require -- To use Modules
+// module -- Info about current module or file
+// process -- info about the environment
+
+
+console.log(__dirname )
+console.log(__filename)
+setInterval(() => {
+    console.log("Hello World!")
+}, 1000)
