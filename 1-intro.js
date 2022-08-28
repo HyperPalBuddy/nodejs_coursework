d4a22e50335c948c21a5a7a7ce574b1935b11056
Saiteja Alapati <61974182+HyperPalBuddy@users.noreply.github.com>
2022-08-28 11:20:05 +0530

    Initial commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const number = 123
+
+if(number>100){
+    console.log("The Number "+number+" Is A Huge Number")
+}
+else {
+    console.log("The Number "+number+" Is A Small Number")
+}
+
+console.log("BTW Hello World!!")
